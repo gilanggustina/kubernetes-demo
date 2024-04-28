77309259ae0cf93aa6aeb97ed7f661afad6caefe
Cahaya Gilang Gustina <cahayagilanggustina01082000@gmail.com>
2024-04-28 18:00:02 +0700

    Bug fix images nodejs writer

--- kubernetes/k8s/images/nodejs-writer/app.js
+++ kubernetes/k8s/images/nodejs-writer/app.js
@@ -3,19 +3,18 @@ const process = require("process");
 let location = process.env.HTML_LOCATION;
 
 if (!location) {
-    location = "/app/html"
+  location = "/app/html";
 }
 
 setInterval(() => {
-    const date = new Date();
-    const html = `<html><body>${date}</body></html>`;
+  const date = new Date();
+  const html = `<html><body>${date}</body></html>`;
 
-    fs.writeFile(location + "/index.html", html, err => {
-        if (err) {
-            console.log("Failed write file")
-        } else {
-            console.log("Success write file")
-        }
-    })
-
+  fs.writeFile(location + "/index.html", html, (err) => {
+    if (err) {
+      console.log("Failed write file");
+    } else {
+      console.log("Success write file");
+    }
+  });
 }, 5000);
